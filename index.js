0a6a4794ec0c85cc61e177a1c1550d9b1bd8c8e7
CodeMagic141 <160157836+CodeMagic141@users.noreply.github.com>
2024-06-23 19:37:42 -0400

    color changes to make less eye strain add js

---
+++ index.js
@@ -0,0 +1,21 @@
+let navslide = document.getElementById("navlink4").onmouseover;
+let isOver = false;
+if(navslide = true)
+{
+    Function()
+    {
+    isOver = document.getElementById("navlink4").value;
+     console.log(typeof isOver);
+    }
+    
+   
+    
+
+}
+let comment;
+
+document.getElementById("submitButton").onclick = function()
+{
+    comment = document.getElemnetByID("Feedback").value;
+    console.log("comment");
+}
